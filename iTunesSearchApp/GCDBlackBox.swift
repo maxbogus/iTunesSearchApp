2c13bea67360d146af49573ea65bc1364c218a34
Max Boguslavskiy <max.bogus@gmail.com>
2018-07-29 13:03:10 +0300

    impletemented draft project structure

---
+++ iTunesSearchApp/GCDBlackBox.swift
@@ -0,0 +1,15 @@
+//
+//  GCDBlackBox.swift
+//  iTunesSearchApp
+//
+//  Created by Max Boguslavskiy on 29/07/2018.
+//  Copyright © 2018 Max Boguslavskiy. All rights reserved.
+//
+
+import Foundation
+
+func performUIUpdatesOnMain(_ updates: @escaping () -> Void) {
+    DispatchQueue.main.async {
+        updates()
+    }
+}
---
+++ iTunesSearchApp/Model/DataController.swift
@@ -0,0 +1,9 @@
+//
+//  DataController.swift
+//  iTunesSearchApp
+//
+//  Created by Max Boguslavskiy on 29/07/2018.
+//  Copyright © 2018 Max Boguslavskiy. All rights reserved.
+//
+
+import Foundation
