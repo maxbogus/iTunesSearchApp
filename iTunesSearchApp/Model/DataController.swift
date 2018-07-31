0ee61122b335970fc29355f577fb063d624a02a5
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-01 01:18:55 +0300

    implemented draft of data controller; attached it to app delegate

--- iTunesSearchApp/Model/DataController.swift
+++ iTunesSearchApp/Model/DataController.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import CoreData
+
+class DataController {
+    let persistantContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistantContainer.viewContext
+    }
+    
+    var backgroundContext: NSManagedObjectContext!
+    
+    init(modelName:String) {
+        persistantContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func configureContexts() {
+        backgroundContext = persistantContainer.newBackgroundContext()
+        
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistantContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            self.autoSaveViewContext()
+            self.configureContexts()
+            completion?()
+        }
+    }
+    
+}
+
+extension DataController {
+    
+    func autoSaveViewContext (interval:TimeInterval = 15) {
+        print("autosaving...")
+        guard interval > 0 else {
+            print("cannot use negative numbers")
+            return
+        }
+        
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
+    
+}
