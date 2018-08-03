ee79004908f96a1fcb3723bc6a6cd7f7ffaf77a3
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-04 02:05:11 +0300

    show modal if no results

--- iTunesSearchApp/View Controllers/SearchResultsViewController.swift
+++ iTunesSearchApp/View Controllers/SearchResultsViewController.swift
@@ -10,17 +10,20 @@ import Foundation
 import UIKit
 
 class SearchResultsViewController: UITableViewController {
+    var results: Array<Any>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if (self.results == nil || self.results.count == 0) {
+            let alert = UIAlertController(title: "Message", message: "No results", preferredStyle: UIAlertControllerStyle.alert)
+            
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        }
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
 }
 
