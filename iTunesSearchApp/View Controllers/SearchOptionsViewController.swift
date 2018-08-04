7e2d84b1c28e9f5a98ea1c0be2b6ad7cc673a9b5
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-05 01:26:45 +0300

    removed unused code

--- iTunesSearchApp/View Controllers/SearchOptionsViewController.swift
+++ iTunesSearchApp/View Controllers/SearchOptionsViewController.swift
@@ -17,17 +17,5 @@ class SearchOptionsViewController: UIViewController {
     
     @IBAction func searchAction(_ sender: Any) {
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
 }
 
