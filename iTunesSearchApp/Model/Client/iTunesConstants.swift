73595456aef7cb462b4363e71d2c695f1ea0bb2b
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-06 01:06:39 +0300

    updated itunes constants

--- iTunesSearchApp/Model/Client/iTunesConstants.swift
+++ iTunesSearchApp/Model/Client/iTunesConstants.swift
@@ -13,52 +13,55 @@ extension iTunesClient {
     // MARK: iTunes
     struct Constants {
         static let APIScheme = "https"
-        static let APIHost = "api.flickr.com"
-        static let APIPath = "/services/rest"
-        
-        static let SearchBBoxHalfWidth = 1.0
-        static let SearchBBoxHalfHeight = 1.0
-        static let SearchLatRange = (-90.0, 90.0)
-        static let SearchLonRange = (-180.0, 180.0)
+        static let APIHost = "itunes.apple.com"
+        static let APIPath = "/"
     }
     
     // MARK: iTunes Parameter Keys
     struct iTunesParameterKeys {
-        static let Method = "method"
-        static let APIKey = "api_key"
-        static let GalleryID = "gallery_id"
-        static let Extras = "extras"
-        static let Format = "format"
-        static let NoJSONCallback = "nojsoncallback"
-        static let SafeSearch = "safe_search"
-        static let Text = "text"
-        static let BoundingBox = "bbox"
-        static let Page = "page"
-        static let PerPage = "per_page"
+        static let Term = "term"
+        static let Country = "country"
+        static let Media = "media"
+        static let Entity = "entity"
+        static let Attribute = "attribute"
+        static let Callback = "callback"
+        static let Limit = "limit"
+        static let Lang = "lang"
+        static let Version = "version"
+        static let Explicit = "explicit"
     }
     
     // MARK: iTunes Parameter Values
     struct iTunesParameterValues {
-        static let SearchMethod = "flickr.photos.search"
-        static let APIKey = "f0dad750eaeb897ef50c8e95846423d7"
-        static let ResponseFormat = "json"
-        static let DisableJSONCallback = "1" /* 1 means "yes" */
-        static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
-        static let GalleryID = "5704-72157622566655097"
-        static let MediumURL = "url_m"
-        static let UseSafeSearch = "1"
-        static let PerPageValue = "20"
+        static let Country = "US"
+        static let Media = "all"
+        static let Entity = "music"
+        static let Attribute = "all" /* 1 means "yes" */
+        static let Limit = "25"
+        static let Lang = "en_us"
+        static let Version = "2"
+        static let Explicit = "No"
     }
     
     // MARK: iTunes Response Keys
     struct iTunesResponseKeys {
-        static let Status = "stat"
-        static let Photos = "photos"
-        static let Photo = "photo"
-        static let Title = "title"
-        static let MediumURL = "url_m"
-        static let Pages = "pages"
-        static let Total = "total"
+        static let Wrapper = "wrapperType"
+        static let TrackExplicitness = "trackExplicitness"
+        static let CollectionExplicitness = "collectionExplicitness"
+        static let Kind = "kind"
+        static let TrackName = "trackName"
+        static let ArtistName = "artistName"
+        static let СollectionName = "collectionName"
+        static let СollectionCensoredName = "collectionCensoredName"
+        static let TrackCensoredName = "trackCensoredName"
+        static let ArtworkUrl100 = "artworkUrl100"
+        static let ArtworkUrl60 = "artworkUrl60"
+        static let ArtistViewUrl = "artistViewUrl"
+        static let CollectionViewUrl = "collectionViewUrl"
+        static let TrackViewUrl = "trackViewUrl"
+        static let PreviewUrl = "previewUrl"
+        static let TrackTimeMillis = "trackTimeMillis"
+        
     }
     
     // MARK: iTunes Response Values
--- iTunesSearchApp/Model/Client/iTunesConvenience.swift
+++ iTunesSearchApp/Model/Client/iTunesConvenience.swift
@@ -13,18 +13,11 @@ import Foundation
 
 extension iTunesClient {
     
-    // MARK: Search photos by coordinates (GET) Methods
-    func searchByLatLon(latitude: Double, longitude: Double, pageNumber: Int, completionHandlerForSession: @escaping (_ success: Bool, _ photosArray: [[String: AnyObject]]?, _ totalPages: Int?, _ errorString: String?) -> Void) {
+    // MARK: Search media by params (GET) Methods
+    func searchByParams(latitude: Double, longitude: Double, pageNumber: Int, completionHandlerForSession: @escaping (_ success: Bool, _ photosArray: [[String: AnyObject]]?, _ totalPages: Int?, _ errorString: String?) -> Void) {
         let methodParameters = [
-            iTunesParameterKeys.Method: iTunesParameterValues.SearchMethod,
-            iTunesParameterKeys.APIKey: iTunesParameterValues.APIKey,
-            iTunesParameterKeys.BoundingBox: bboxString(latitude: latitude, longitude: longitude),
-            iTunesParameterKeys.SafeSearch: iTunesParameterValues.UseSafeSearch,
-            iTunesParameterKeys.Extras: iTunesParameterValues.MediumURL,
-            iTunesParameterKeys.Format: iTunesParameterValues.ResponseFormat,
-            iTunesParameterKeys.NoJSONCallback: iTunesParameterValues.DisableJSONCallback,
-            iTunesParameterKeys.Page: "\(pageNumber)",
-            iTunesParameterKeys.PerPage: iTunesParameterValues.PerPageValue
+            iTunesParameterKeys.Lang: iTunesParameterValues.Lang,
+            iTunesParameterKeys.Limit: iTunesParameterValues.Limit,
         ]
         let _ = taskForGETMethod(parameters: methodParameters as [String : AnyObject]) { (results, error) in
             
@@ -47,12 +40,31 @@ extension iTunesClient {
         }
     }
     
-    private func bboxString(latitude: Double, longitude: Double) -> String {
-        // ensure bbox is bounded by minimum and maximums
-        let minimumLon = max(longitude - Constants.SearchBBoxHalfWidth, Constants.SearchLonRange.0)
-        let minimumLat = max(latitude - Constants.SearchBBoxHalfHeight, Constants.SearchLatRange.0)
-        let maximumLon = min(longitude + Constants.SearchBBoxHalfWidth, Constants.SearchLonRange.1)
-        let maximumLat = min(latitude + Constants.SearchBBoxHalfHeight, Constants.SearchLatRange.1)
-        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+    // MARK: Lookup by params (GET) Methods
+    func lookupByParams(latitude: Double, longitude: Double, pageNumber: Int, completionHandlerForSession: @escaping (_ success: Bool, _ photosArray: [[String: AnyObject]]?, _ totalPages: Int?, _ errorString: String?) -> Void) {
+        let methodParameters = [
+            iTunesParameterKeys.Lang: iTunesParameterValues.Lang,
+            iTunesParameterKeys.Limit: iTunesParameterValues.Limit,
+        ]
+        let _ = taskForGETMethod(parameters: methodParameters as [String : AnyObject]) { (results, error) in
+            
+            /* 3. Send the desired value(s) to completion handler */
+            if error != nil {
+                completionHandlerForSession(false, nil, nil, "Search failed.")
+            } else {
+                if let photosDictionary = results?[iTunesResponseKeys.Photos] as? NSDictionary {
+                    if let photosArray = photosDictionary[iTunesResponseKeys.Photo] as? [[String: AnyObject]], let totalPages = photosDictionary[iTunesResponseKeys.Pages] as? Int {
+                        completionHandlerForSession(true, photosArray, totalPages, nil)
+                    } else {
+                        print("Could not find \(iTunesResponseKeys.Photo) in \(results!)")
+                        completionHandlerForSession(false, nil, nil, "Get photo array failed.")
+                    }
+                } else {
+                    print("Could not find \(iTunesResponseKeys.Photos) in \(results!)")
+                    completionHandlerForSession(false, nil, nil, "Get photos dictionary failed.")
+                }
+            }
+        }
     }
+
 }
