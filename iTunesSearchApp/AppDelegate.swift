67230f8af7ad8d27446a02ffc4e25290b801f6a3
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-08 01:23:47 +0300

    fixed loading of data model

--- iTunesSearchApp/AppDelegate.swift
+++ iTunesSearchApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    let dataController = DataController(modelName: "iTunesSearch")
+    let dataController = DataController(modelName: "iTunesSearchApp")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         dataController.load()
