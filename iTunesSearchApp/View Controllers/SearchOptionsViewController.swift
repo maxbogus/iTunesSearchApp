644f482a428cca737af2c9d0bcaf636bf276d011
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-09 01:57:12 +0300

    added close on enter to term input; added hide/show slide effect to limit window; added parsing of vars

--- iTunesSearchApp/View Controllers/SearchOptionsViewController.swift
+++ iTunesSearchApp/View Controllers/SearchOptionsViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class SearchOptionsViewController: UIViewController {
+class SearchOptionsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var countryList: UITableView!
     @IBOutlet var explicitOption: UISwitch!
     @IBOutlet var limitResults: UITextField!
@@ -17,22 +17,80 @@ class SearchOptionsViewController: UIViewController {
     @IBOutlet var termInput: UITextField!
     
     @IBAction func searchAction(_ sender: Any) {
-        iTunesClient.sharedInstance.searchByParams(term: "john", limit: 25, explicitness: true) { (completed, results, resultsCount, error) in
-            if completed {
-                if let results = results, let count = resultsCount {
-                    print(results)
-                    print(count)
+        if let limit = Int(limitResults.text!), let term = termInput.text {
+            iTunesClient.sharedInstance.searchByParams(term: term, limit: limit, explicitness: explicitOption.isOn) { (completed, results, resultsCount, error) in
+                if completed {
+                    if let results = results, let count = resultsCount {
+                        print(results)
+                        print(count)
+                    }
+                } else {
+                    let alert = UIAlertController(title: "Error", message: "\(String(describing: error))", preferredStyle: UIAlertControllerStyle.alert)
+                    
+                    // add an action (button)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    
+                    // show the alert
+                    self.present(alert, animated: true, completion: nil)
                 }
-            } else {
-                let alert = UIAlertController(title: "Error", message: "\(String(describing: error))", preferredStyle: UIAlertControllerStyle.alert)
-                
-                // add an action (button)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                
-                // show the alert
-                self.present(alert, animated: true, completion: nil)
             }
+        } else {
+            print(error)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        limitResults.delegate = self
+        termInput.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+//        if termInput.isFirstResponder {
+//            view.frame.origin.y -= self.getKeyboardHeight(notification)
+//        }
+        if limitResults.isFirstResponder {
+            view.frame.origin.y -= self.getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+//        if termInput.isFirstResponder {
+//            view.frame.origin.y = 0
+//        }
+        if limitResults.isFirstResponder {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    @objc func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
     }
 }
 
