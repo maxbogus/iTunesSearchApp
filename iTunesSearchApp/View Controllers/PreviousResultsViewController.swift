4dba05b573509952acc71283b0b5ea5e1f52fd66
Max Boguslavskiy <max.bogus@gmail.com>
2018-08-17 00:46:29 +0300

    fixed max number of search options

--- iTunesSearchApp/View Controllers/PreviousResultsViewController.swift
+++ iTunesSearchApp/View Controllers/PreviousResultsViewController.swift
@@ -29,17 +29,17 @@ class PreviousResultsViewController: UITableViewController, UITextFieldDelegate 
         }
     }
     
-    override func viewDidLoad() {
-        dataController = AppDelegate.sharedInstance.dataController
-        setUpFetchedResultsController()
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        dataController = AppDelegate.sharedInstance.dataController
+        setUpFetchedResultsController()
 
         DispatchQueue.main.async {
             if let searchOptions = self.loadSearchOptions() {
                 self.previousResults = searchOptions
+                self.tableView.reloadData()
+            } else {
+                self.previousResults = nil
                 self.tableView.reloadData()
             }
 
@@ -70,25 +70,47 @@ class PreviousResultsViewController: UITableViewController, UITextFieldDelegate 
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchedResultsController.sections?.count ?? 1
+        return fetchedResultsController?.sections?[0].objects!.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "previousSearchRequest", for: indexPath)
-        let item = fetchedResultsController.object(at: indexPath)
-        cell.detailTextLabel?.text = item.description
-        cell.textLabel?.text = "\(describing: item.creationDate?.description)"
+        
+        if self.validateIndexPath(indexPath) {
+            let item = fetchedResultsController.object(at: indexPath)
+            cell.detailTextLabel?.text = item.description
+            cell.textLabel?.text = "\(describing: item.creationDate?.description)"
+        } else {
+            print("Attempting to configure a cell for an indexPath that is out of bounds: \(indexPath)")
+        }
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let indexPath = tableView.indexPathForSelectedRow //optional, to get from any UIButton for example
-
         let currentCell = tableView.cellForRow(at: indexPath!)
-        print(fetchedResultsController.object(at: indexPath!))
+        
+        if self.validateIndexPath(indexPath!) {
+            print(fetchedResultsController.object(at: indexPath!))
+        } else {
+            print("Attempting to configure a cell for an indexPath that is out of bounds: \(String(describing: indexPath))")
+        }
+
         print((currentCell?.textLabel?.text)!)
         print((currentCell?.detailTextLabel?.text)!)
+
 //        self.country = (currentCell?.textLabel?.text)!
+    }
+    
+    func validateIndexPath(_ indexPath: IndexPath) -> Bool {
+        if let sections = self.fetchedResultsController?.sections,
+            indexPath.section < sections.count {
+            if indexPath.row < sections[indexPath.section].numberOfObjects {
+                return true
+            }
+        }
+        return false
     }
 }
 
